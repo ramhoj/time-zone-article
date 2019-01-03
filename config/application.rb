7d600b7130f771ac573d66bf86712f9956ea1cba
Nicklas Ramhöj <nicklas.ramhoj@varvet.com>
2019-01-03 15:42:19 +0100

    Update dependencies

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,7 @@ module TimeZoneArticle
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = "Kabul" # UTC +04:30
+
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
